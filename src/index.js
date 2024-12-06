4616ae452161ff8513e4b3b22ae156103910995f
srijan <srijan.dhl21@gmail.com>
2024-12-07 02:40:42 +1100

    done workingcapitalratio

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Expenses } from "./metrics/Expenses.js";
 import { GrossProfitMargin } from "./metrics/GrossProfitMargin.js";
 import { NetProfitMargin } from "./metrics/NetProfitMargin.js";
 import { Revenue } from "./metrics/Revenue.js";
+import { WorkingCapitalRatio } from "./metrics/WorkingCapitalRatio.js";
 
 Revenue((err, revenue) => {
   if (err) {
@@ -12,22 +13,29 @@ Revenue((err, revenue) => {
 });
 Expenses((err, expenses) => {
   if (err) {
-    console.error("Error calculating revenue:", err);
+    console.error("Error calculating expenses:", err);
   } else {
     console.log("Total Expenses:", expenses);
   }
 });
 GrossProfitMargin((err, grossprofitmargin) => {
   if (err) {
-    console.error("Error calculating revenue:", err);
+    console.error("Error calculating Gross Profit Margin:", err);
   } else {
     console.log("Gross Profit Margin:", grossprofitmargin);
   }
 });
 NetProfitMargin((err, netprofitmargin) => {
   if (err) {
-    console.error("Error calculating netprofitmargin:", err);
+    console.error("Error calculating Net Profit Margin:", err);
   } else {
     console.log("Net Profit Margin:", netprofitmargin);
+  }
+});
+WorkingCapitalRatio((err, workingcapitalratio) => {
+  if (err) {
+    console.error("Error calculating Working Capital Ratio", err);
+  } else {
+    console.log("Working Capital Ratio:", workingcapitalratio);
   }
 });
--- src/metrics/WorkingCapitalRatio.js
+++ src/metrics/WorkingCapitalRatio.js
@@ -1,3 +1,60 @@
-export const WorkingCapitalRation = () => {
+import { ReadDataFile } from "../util/ReadDataFile.js";
+
+export const WorkingCapitalRatio = (callback) => {
   const filepath = "src/data/data.json";
+  ReadDataFile(filepath, (err, data) => {
+    if (err) {
+      console.log("Error reading file:", err);
+      callback(err, null);
+      return;
+    }
+    const debitAssets = data
+      .filter(
+        (item) =>
+          item.account_category === "assets" &&
+          item.value_type === "debit" &&
+          (item.account_type === "current" ||
+            item.account_type === "bank" ||
+            item.account_type === "current_accounts_receivable")
+      )
+      .map((item) => item.total_value)
+      .reduce((sum, value) => sum + value, 0);
+    const creditAssets = data
+      .filter(
+        (item) =>
+          item.account_category === "assets" &&
+          item.value_type === "credit" &&
+          (item.account_type === "current" ||
+            item.account_type === "bank" ||
+            item.account_type === "current_accounts_receivable")
+      )
+      .map((item) => item.total_value)
+      .reduce((sum, value) => sum + value, 0);
+    const assets = debitAssets - creditAssets;
+
+    const debitLiabilities = data
+      .filter(
+        (item) =>
+          item.account_category === "liability" &&
+          item.value_type === "debit" &&
+          (item.account_type === "current" ||
+            item.account_type === "current_accounts_payable")
+      )
+      .map((item) => item.total_value)
+      .reduce((sum, value) => sum + value, 0);
+    const creditLiabilities = data
+      .filter(
+        (item) =>
+          item.account_category === "liability" &&
+          item.value_type === "credit" &&
+          (item.account_type === "current" ||
+            item.account_type === "current_accounts_payable")
+      )
+      .map((item) => item.total_value)
+      .reduce((sum, value) => sum + value, 0);
+
+    const liabilities = creditLiabilities - debitLiabilities;
+    const workingcapitalratio = (assets / liabilities) * 100;
+    callback(null, workingcapitalratio);
+  });
 };
