cc2e37620cb9ebbdb964333b9451e11b8ffc19dd
Diego Scarpati <diegoscarpati13@gmail.com>
2024-12-07 09:47:31 +1000

    Initialized React Next project and defined types from JSON data

---
+++ src/app/types/enums.ts
@@ -0,0 +1,12 @@
+export enum AccountCategory {
+  REVENUE = "revenue",
+  EXPENSE = "expense",
+  ASSETS = "assets",
+  LIABILITY = "liability"
+}
+
+export enum ValueType {
+  CREDIT = "credit",
+  DEBIT = "debit"
+}
+
