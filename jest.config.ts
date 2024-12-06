decfbc4ecbccaedaf374830c54c7d76864f1ca24
abishkar rai <raiabishkar0.5@gmail.com>
2024-12-06 18:58:48 +1100

    test cases are done

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    globals: {
+      'ts-jest': {
+        isolatedModules: true, 
+      },
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  };
+  
