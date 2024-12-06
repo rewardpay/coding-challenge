84b8cea04fffb0afe613a5c5ca2c8b50a77e6e64
chandana89 <nagachandana.mudusu@gmail.com>
2024-12-06 16:13:15 +1100

    calculate gross profit margin

--- calculateMetrics.js
+++ calculateMetrics.js
@@ -1,15 +1,9 @@
-const formatCurrency = (value) => {
-    return `$${Math.round(value).toLocaleString()}`;
-};
-
-
 exports.calculateRevenue = (data) => {
     const revenue = data
                     .filter(item => item.account_category === 'revenue')
                     .reduce((sum, item) => sum + item.total_value, 0);
 
-    const formattedRevenue = formatCurrency(revenue);
-    return formattedRevenue;
+    return revenue;
    
 };
 
@@ -18,8 +12,17 @@ exports.calculateExpenses = (data) => {
                     .filter(item => item.account_category === 'expense')
                     .reduce((sum, item) => sum + item.total_value, 0);
 
-    const formattedExpenses = formatCurrency(expenses);
-    return formattedExpenses;
+    return expenses;
+};
+
+exports.calculateGrossProfit = (data, revenue) => {
+   
+    const salesDebitValue = data
+                    .filter(item => item.account_type === 'sales' && item.value_type === 'debit')
+                    .reduce((sum, item) => sum + item.total_value, 0);
+   
+    const grossProfit = (salesDebitValue / revenue) * 100;
+    return grossProfit;
 };
   
 
--- main.js
+++ main.js
@@ -3,10 +3,21 @@ const metrics = require("./calculateMetrics");
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 
+const formatCurrency = (value) => {
+    return `$${Math.round(value).toLocaleString()}`;
+};
+
+const formatPercentage = (value) => {
+    return `${value.toFixed(1)}%`;
+};
+
 const revenue = metrics.calculateRevenue(data.data);
 const expenses = metrics.calculateExpenses(data.data);
-console.log("Revenue:",revenue);
-console.log("Exoenses:",expenses);
+const grossProfitMargin = metrics.calculateGrossProfit(data.data,revenue);
+
+console.log("Revenue:",formatCurrency(revenue));
+console.log("Expenses:",formatCurrency(expenses));
+console.log("Gross Profit Margin:",formatPercentage(grossProfitMargin));
 
 
   
