0f8f4dad01ecafd355a22c2651893f7400044985
chandana89 <nagachandana.mudusu@gmail.com>
2024-12-06 16:54:49 +1100

    calculate working capital ratio

--- main.js
+++ main.js
@@ -15,11 +15,12 @@ const revenue = metrics.calculateRevenue(data.data);
 const expenses = metrics.calculateExpenses(data.data);
 const grossProfitMargin = metrics.calculateGrossProfit(data.data, revenue);
 const netProfitMargin = metrics.calculateNetProfit(revenue, expenses);
+const workingCapitalRatio = metrics.calculateWorkingCapitalRatio(data.data);
 
 console.log("Revenue:",formatCurrency(revenue));
 console.log("Expenses:",formatCurrency(expenses));
 console.log("Gross Profit Margin:",formatPercentage(grossProfitMargin));
 console.log("Net Profit Margin:",formatPercentage(netProfitMargin));
-
+console.log("Working Capital Ratio:",formatPercentage(workingCapitalRatio));
 
   
