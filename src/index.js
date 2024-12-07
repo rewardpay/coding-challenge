a0ff895e2fb8188e30f4904d559f3662bc1328fd
Kateryna Bilokur <kotiku@gmail.com>
2024-12-07 15:30:58 +1100

    Saving calculations to the file

--- src/index.js
+++ src/index.js
@@ -25,22 +25,29 @@ function main() {
     // balance_date: '2020-09-30T00:00:00.000Z'
     const { data } = readJsonFile(dataFilePath);
 
+    //Clear previously stored values from the file
+    clearFile();
+
+    //Calculate and save Revenue value
     const revenue = calculateRevenue(data);
+    appendToFile(`Revenue: ${formatCurrency(revenue)}`);
+
+    //Calculate and save Expenses value
     const expenses = calculateExpenses(data);
+    appendToFile(`Expenses: ${formatCurrency(expenses)}`);
+
+    //Calculate and save Gross Profit Margin value
     const grossProfMarg = calculateGrossProfitMargin(data, revenue);
+    appendToFile(`Gross Profit Margin: ${formatPercentage(grossProfMarg)}`);
+
+    //Calculate and save Net Profit Margin value
     const netProfMarg = calculateNetProfitMargin(data, revenue, expenses);
+    appendToFile(`Net Profit Margin: ${formatPercentage(netProfMarg)}`);
 
     console.log(formatCurrency(revenue));
     console.log(formatCurrency(expenses));
     console.log(formatPercentage(grossProfMarg));
     console.log(formatPercentage(netProfMarg));
-
-    console.log(+"tete");
-
-    //
-
-    // clearFile();
-    // appendToFile("Some text comes here.");
   } catch (error) {
     console.error("Failed to load data:", error.message);
   }
