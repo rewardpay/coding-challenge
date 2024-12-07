af62a682f35a074e85ed2a2de63621194292aae6
Milan Ghimire <milanghimire656@gmail.com>
2024-12-07 18:52:45 +1100

    Feature/gross profit margin calculation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { parseData, formatCurrency } from '../utils/utils';
 import { calculateRevenue } from './metrics/revenue-calculation';
 import { calculateExpenses } from './metrics/expenses-calculation';
+import { calculateGrossProfitMargin } from './metrics/gross-profit-margin-calculation';
 
 const data = parseData('./data/data.json');
 //console.log(data);  // to check the parsed data to verify it's an array
@@ -11,6 +12,10 @@ const revenue = calculateRevenue(data);
 // Calculate expenses using the expenses-calculation.ts function
 const expenses = calculateExpenses(data);
 
+// Calculate expenses using the gross-profit-margin-calculation.ts function
+const grossProfitMargin = calculateGrossProfitMargin(data, revenue);
+
 console.log(`Revenue: ${formatCurrency(revenue)}`);
 console.log(`Expenses: ${formatCurrency(expenses)}`);
+console.log(`Gross Profit Margin: ${grossProfitMargin.toFixed(1)}%`);
 
