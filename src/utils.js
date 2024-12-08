fca3eb482240d2a9169f0e3fb9c8c05729f4461d
Ranjan <ranjan.mndr@gmail.com>
2024-12-08 14:25:28 +1100

    add format percentage func

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export function formatCurrency(value) {
   return `$${value.toLocaleString(undefined, { maximumFractionDigits: 0 })}`;
 }
+
+export function formatPercentage(value) {
+  return `${(value * 100).toFixed(1)}%`;
+}
